458843336f19fc9048b54df4a9718dd948cafacc
Sampo Kupiainen <sampokupiainen@gmail.com>
2024-02-23 17:38:14 +0200

    New button layout with working scripts

--- Assets/Scripts/CloseGameButton.cs
+++ Assets/Scripts/CloseGameButton.cs
@@ -1,18 +1,8 @@
 using UnityEngine;
-using UnityEngine.UI;
 
 public class CloseGameButton : MonoBehaviour
 {  
-    // Kenttä, johon vedetään Close-painike.
-    public Button closeButton;
-
-    // Vaihtaa scenen pelaajan liikkuessa siirtymäalueelle.
-    private void ButtonOnClick()
-    {
-        closeButton.onClick.AddListener(DoOnClick);
-    }
-
-    private void DoOnClick()
+    public void CloseGame()
     {
         Application.Quit();
     }
