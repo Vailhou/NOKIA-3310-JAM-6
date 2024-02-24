5ae20b715248fe45244e21c6cf311f2d1ea73535
Sampo Kupiainen <sampokupiainen@gmail.com>
2024-02-24 14:35:17 +0200

    End screen buttons working

--- Assets/Scripts/ReplayButton.cs
+++ Assets/Scripts/ReplayButton.cs
@@ -6,16 +6,9 @@ public class ReplayButton : MonoBehaviour
 {  
     // Kenttä, johon syötetään seuraavan scenen ID.
     public string nextSceneName;
-    // Kenttä, johon vedetään replay-painike.
-    public Button replayButton;
 
-    // Vaihtaa scenen pelaajan liikkuessa siirtymäalueelle.
-    private void ButtonOnClick()
-    {
-        replayButton.onClick.AddListener(DoOnClick);
-    }
-
-    private void DoOnClick()
+    // Aloittaa pelin 1. taosta.
+    public void RestartGame()
     {
         SceneManager.LoadScene(nextSceneName);
     }
